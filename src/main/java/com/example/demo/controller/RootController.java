347059bc4405854cd6cb2e5867f8e14ccd7e50de
HailAngel2 <angelpimiento15@gmail.com>
2025-11-22 22:28:48 -0300

    RootController.java

---
+++ src/main/java/com/example/demo/controller/RootController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public ResponseEntity<Map<String, String>> home() {
+        Map<String, String> response = new HashMap<>();
+        
+        response.put("status", "API Running OK");
+        response.put("service", "EV3 FullStack Ventas Backend");
+        response.put("version", "0.0.1-SNAPSHOT");
+        response.put("documentation", "/swagger-ui/index.html");
+        response.put("api_prefix", "/api/");
+        
+        return ResponseEntity.ok(response);
+    }
+}
